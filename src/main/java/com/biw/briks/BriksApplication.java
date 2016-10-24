bca0abbf498b9f914533eef133fd891db5f0949d
Kancherla, Ravi <Ravi.Kancherla@biworldwide.com>
2016-10-24 14:54:36 -0500

    Application.java

--- src/main/java/com/biw/briks/BriksApplication.java
+++ src/main/java/com/biw/briks/BriksApplication.java
@@ -1,14 +1,40 @@
 
 package com.biw.briks;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Arrays;
 
-@SpringBootApplication
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration( exclude = { DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class } )
+@ComponentScan( basePackages = { "com.biw.briks" } )
 public class BriksApplication
 {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger( BriksApplication.class );
+
   public static void main( String[] args )
   {
-    SpringApplication.run( BriksApplication.class, args );
+    LOGGER.info( "Starting Briks application" );
+    ConfigurableApplicationContext ctx = SpringApplication.run( BriksApplication.class, args );
+
+    LOGGER.debug( "Let's inspect the beans provided by Spring Boot:" );
+
+    String[] beanNames = ctx.getBeanDefinitionNames();
+    Arrays.sort( beanNames );
+    for ( String beanName : beanNames )
+    {
+      LOGGER.debug( beanName );
+    }
   }
+
 }

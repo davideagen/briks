7689852d109ca5e6b11a818ab593d85799da1e0b
Kancherla, Ravi <Ravi.Kancherla@biworldwide.com>
2016-10-24 14:10:28 -0500

    Initial commit for spring boot configuration

---
+++ src/main/java/com/biw/briks/BriksApplication.java
@@ -0,0 +1,12 @@
+package com.biw.briks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BriksApplication.class, args);
+	}
+}
